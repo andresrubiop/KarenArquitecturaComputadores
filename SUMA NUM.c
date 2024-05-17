de62fff89230234c6a73f6e37a10027f020e6d21
Karen Cabrerae <karencabrera2992@gmail.com>
2024-05-17 12:22:10 -0500

    lll

---
+++ SUMA NUM.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main ()
+{
+    int Num1=0;
+    int Num2=0;
+    int Sum=0;
+
+    printf ("\tSUMA DE DOS NUMEROS\n");
+    printf ("Ingrese el primer numero: %d\n", Num1);
+    scanf ("%d", Num1);
+    printf ("Ingrese el segundo numero: %d\n", Num2);
+    scanf ("%d", Num2);
+
+    Sum=Num1+Num2;
+
+    printf ("\n\tLa suma es: %d\n", Sum);
+
+    int I=4;
+    
+
+    
+ 
+    
+}
+
+
---
+++ SUMA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main ()
+{
+    
+
+    int Sum=1+1;
+    int Sum2=2+2;
+    
+    
+}
+
+
